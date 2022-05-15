d889628fc19dcaa4b566563f153e08844ee3d053
AdnanK1 <adnangitonga@gmail.com>
2022-05-15 16:02:47 +0300

    Added routing to both login and register page

--- app/main/views.py
+++ app/main/views.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template,redirect,url_for
 from . import main
 
 @main.route('/')
@@ -6,3 +6,13 @@ from . import main
 def index_page():
 
     return render_template('index.html')
+
+@main.route('/register')
+def register_page():
+
+    return render_template('register.html')
+
+@main.route('/login')
+def login_page():
+
+    return render_template('login.html')
