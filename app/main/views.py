a13d3b061dcb861fb48c009ab817c7c04e8aa356
AdnanK1 <adnangitonga@gmail.com>
2022-05-15 16:21:15 +0300

    allowed the register form to take in data and have it post to the database in the view file

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,8 @@
 from flask import render_template,redirect,url_for
 from . import main
+from .forms import RegisterForm,LoginForm
+from ..models import User
+from ..extensions import db
 
 @main.route('/')
 @main.route('/home')
@@ -7,10 +10,19 @@ def index_page():
 
     return render_template('index.html')
 
-@main.route('/register')
+@main.route('/register', methods=['GET', 'POST'])
 def register_page():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        username = form.username.data
+        email = form.email.data
+        password = form.password1.data
+        new_user = User(username=username,email=email,password=password)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('main.index_page'))
 
-    return render_template('register.html')
+    return render_template('register.html',form=form)
 
 @main.route('/login')
 def login_page():
