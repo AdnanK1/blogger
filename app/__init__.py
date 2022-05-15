ac25f398749af1bdbee1b49937f5d13f3f040970
AdnanK1 <adnangitonga@gmail.com>
2022-05-15 11:26:48 +0300

    Added the tables required for the database

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,16 @@
 from flask import Flask
 from .main import views
 from config import config_options
+from .extensions import db,b_crypt,login_manager
 
 def create_app(config_name):
     app = Flask(__name__)
 
     #Initializing flask extensions
-
+    db.init_app(app)
+    b_crypt.init_app(app)
+    login_manager.init_app(app)
+    
     #Creating the app configurations
     app.config.from_object(config_options[config_name])
 
