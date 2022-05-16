4b9d869fc11daa4247a8c32d1baab05370de4421
AdnanK1 <adnangitonga@gmail.com>
2022-05-16 15:56:45 +0300

    Added a relationship between blogs table to comments table by giving a foreign key to blogs table

--- app/models.py
+++ app/models.py
@@ -30,9 +30,12 @@ class Blog(db.Model):
     id = db.Column(db.Integer(),primary_key=True)
     blog = db.Column(db.String(),nullable=False)
     author = db.Column(db.Integer(),db.ForeignKey('users.id'))
+    blogs = db.Column(db.Integer(),db.ForeignKey('comments.id'))
 
 class Comment(db.Model):
     __tablename__ = 'comments'
     id = db.Column(db.Integer(), primary_key = True)
     comment = db.Column(db.String(),nullable=False)
     authors = db.Column(db.Integer(),db.ForeignKey('users.id'))
+    blog_id = db.relationship('Blog',backref='comments')
+    
