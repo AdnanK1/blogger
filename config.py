47709ff6d46ffd179169e516a702a1879b382047
AdnanK1 <adnangitonga@gmail.com>
2022-05-17 23:36:30 +0300

    Changed database uri

--- config.py
+++ config.py
@@ -6,14 +6,13 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     
 class ProdConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI='postgresql://zkegooymmmwpcz:acdca99c384267dc807e7224c8d4a458b782b33b406542ec5823ca992e4adbab@ec2-3-229-11-55.compute-1.amazonaws.com:5432/d6acpdt1d37rth'
     
-
-
+    
 class DevConfig(Config):
     
     DEBUG = True
-
+    pass
 config_options = {
     'development':DevConfig,
     'production':ProdConfig
